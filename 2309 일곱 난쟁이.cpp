f4af40c689bbe5a895872d047beac93e324100c4
YUDAHAN <dbekgks98@gmail.com>
2023-09-02 15:34:03 +0900

    combinations
    combinations

--- 2309 일곱 난쟁이.cpp
+++ 2309 일곱 난쟁이.cpp
@@ -1,13 +1,35 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std;  
 int a[9], sum;
+vector<int> v;
+pair<int, int> ret;
+void solve() {
+	for(int i = 0; i < 9; i++) {
+		for(int j = 0; j < i; j++) {
+			if(100 == sum - a[i] - a[j]) {
+				ret.first = i;
+				ret.second = j;
+				return;
+			}
+		}
+	}
+}
 int main() {
-	for (int i = 0; i < 9; i++) cin >> a[i];
-	sort(a, a + 9);
-	do {
-		sum = 0;
-		for(int i = 0; i < 7; i++) sum+=a[i];
-		if(sum == 100) break;
-	} while(next_permutation(a, a + 9));
-	for (int i = 0; i < 7; i++) cout << a[i] << '\n';
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	for (int i = 0; i < 9; i++) {
+		cin >> a[i];	
+		sum += a[i];
+	}
+	
+	solve();
+	for (int i = 0; i < 9; i++) {
+		if (i == ret.first || i == ret.second) continue;
+		v.push_back(a[i]);
+	}
+	sort(v.begin(), v.end());
+	for (int i : v) cout << i << '\n';
+	return 0;
 }
